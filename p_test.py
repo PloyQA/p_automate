4837b4e1b179309191c5f07d52ce9aa6c185a463
ploy.saetung@allianz.com <ploysaetung@gmail.com>
2021-09-07 15:02:33 +0700

    Comment in code #2

--- p_test.py
+++ p_test.py
@@ -10,7 +10,18 @@ def showHelloAllUser():         #Define Function
 
 customers = [] #set variable (list)
 user_input = int(input()) #Get data
-inputNameofAllUser(user_input)
-showHelloAllUser()
+inputNameofAllUser(user_input)  #Call function and send data
+showHelloAllUser()              #Call function
 
+#------------------Output------------------
+#Name of User 0 : AA
+#['AA']
+#Name of User 1 : ZX
+#['AA', 'ZX']
 
+#Hello AA
+#--------
+
+#Hello ZX
+#--------
+#-----------------------------------------
