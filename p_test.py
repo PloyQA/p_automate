99db2726303de67efc458454b608e1e0bdc1f40e
ploy.saetung@allianz.com <ploysaetung@gmail.com>
2021-09-07 14:58:34 +0700

    Start Create Python Script #1

--- p_test.py
+++ p_test.py
@@ -1,4 +1,16 @@
-print('Hello World')
-print('Hello World 2')
-print('Hello World 3')
-print('Hello World 4')
+#print('Hello World')
+
+def inputNameofAllUser(round): #Define Function
+    for index in range(round):
+        customers.append(input("Name of User "+ str(index) +" : "))
+        print(customers)
+def showHelloAllUser():         #Define Function
+    for data in customers:
+        print("\nHello",data,"\n--------")
+
+customers = [] #set variable (list)
+user_input = int(input()) #Get data
+inputNameofAllUser(user_input)
+showHelloAllUser()
+
+
